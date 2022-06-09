75bc8d39829e6e542f75e1a7eceb5b90c0903645
Amirhosien <zr.amirhosein@gmail.com>
2022-06-09 15:58:43 +0430

    Add init files

---
+++ src/main/java/com/example/trafficplan/TrafficPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.trafficplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trafficplan/TrafficPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trafficplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
